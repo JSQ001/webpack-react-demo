a40e829d756b3b76a614398a96b9de906498d25a
jsq <1647013149@qq.com>
2018-08-22 18:21:22 +0800

    di

---
+++ src/client.js
@@ -0,0 +1,20 @@
+import _ from 'lodash'; // lodash 是由当前 script 脚本 import 导入进来的
+import 'styles/common.scss';
+import Main from 'containers/main.js'
+import BG from 'images/BG.jpg';
+
+const component=()=>{
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    element.id = 'root';
+
+    // 将图像添加到我们现有的 div。
+    const myIcon = new Image();
+    myIcon.src = BG;
+    element.appendChild(myIcon);
+    return element;
+};
+
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* eslint no-console: "off" */
+const webpackConfigs = require('./config/webpack');
+const defaultConfig = 'test';
+
+module.exports = (configName) => {
+
+  console.log(configName)
+
+  // 如果没有给定环境，默认dev
+  const requestedConfig = configName || defaultConfig;
+
+  // 如果给定的环境不存在，默认dev
+  let LoadedConfig = defaultConfig;
+
+  if (webpackConfigs[requestedConfig] !== undefined) {
+    LoadedConfig = webpackConfigs[requestedConfig];
+  } else {
+    console.warn(`
+      Provided environment "${configName}" was not found.
+      Please use one of the following ones:
+      ${Object.keys(webpackConfigs).join(' ')}
+    `);
+    LoadedConfig = webpackConfigs[defaultConfig];
+  }
+
+  const loadedInstance = new LoadedConfig();
+
+  // Set the global environment
+  process.env.NODE_ENV = loadedInstance.env;
+
+  return loadedInstance.config;
+};
